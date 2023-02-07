c2f159eaadac857060b5b7441b22bd8a8b0562b7
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-02-07 14:27:17 +0200

    hw

---
+++ src/test/java/lesson_1/HW.java
@@ -0,0 +1,34 @@
+package lesson_1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HW {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //Откройте сайт: https://www.amazon.com/
+        driver.get("https://www.amazon.com/");
+        //Выведите в консоль:
+        //Адрес сайта
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+        //Заголовок сайта
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        //Откройте сайт https://www.walmart.com/
+        driver.navigate().to("https://www.walmart.com/");
+        //Вернитесь на первый сайт.
+        driver.navigate().back();
+        //Вернитесь на второй сайт.
+        driver.navigate().forward();
+        //Перезагрузите второй сайт.
+        driver.navigate().refresh();
+        //На втором сайте выведите в консоль:
+        //Адрес сайта
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+        //Заголовок сайта
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+    }
+}
